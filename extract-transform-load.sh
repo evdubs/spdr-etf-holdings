fcb4df04faac19f6ee3aea72323007cde1da182f
evdubs <1124030+evdubs@users.noreply.github.com>
2021-10-01 10:37:00 -1000

    remove XTH from ETF list.

--- extract-transform-load.sh
+++ extract-transform-load.sh
@@ -4,7 +4,7 @@ today=$(date "+%F")
 dir=$(dirname "$0")
 current_year=$(date "+%Y")
 
-racket ${dir}/extract.2020-01-01.rkt
+racket ${dir}/extract.2021-10-01.rkt
 racket ${dir}/transform-load-csv.2019-11-02.rkt -c -p "$1"
 
 7zr a /var/tmp/spdr/etf-holdings/${current_year}.7z /var/tmp/spdr/etf-holdings/${today}
---
+++ extract.2021-10-01.rkt
@@ -0,0 +1,43 @@
+#lang racket/base
+
+(require gregor
+         net/http-easy
+         racket/file
+         racket/list
+         racket/port
+         tasks
+         threading)
+
+(define (download-etf-holdings symbol)
+  (make-directory* (string-append "/var/tmp/spdr/etf-holdings/" (~t (today) "yyyy-MM-dd")))
+  (call-with-output-file (string-append "/var/tmp/spdr/etf-holdings/" (~t (today) "yyyy-MM-dd") "/" symbol ".xls")
+    (λ (out)
+      (with-handlers ([exn:fail?
+                       (λ (error)
+                         (displayln (string-append "Encountered error for " symbol))
+                         (displayln ((error-value->string-handler) error 1000)))])
+        (~> (string-append "https://www.ssga.com/library-content/products/fund-data/etfs/us/holdings-daily-us-en-"
+                           (string-downcase symbol) ".xlsx")
+            (get _)
+            (response-body _)
+            (write-bytes _ out))))
+    #:exists 'replace))
+
+(define spdr-etfs (list
+                   ; Core ETFs
+                   "DIA" "MDY" "SLY" "SPY"
+                   ; Sector ETFs
+                   "XLB" "XLC" "XLE" "XLF" "XLI" "XLK" "XLP" "XLRE" "XLU" "XLV" "XLY"
+                   ; Industry (Modified Equal Weighted) ETFs
+                   "KBE" "KCE" "KIE" "KRE" "XAR" "XBI" "XES" "XHB" "XHE" "XHS" "XME" "XOP" "XPH" "XRT" "XSD" "XSW" "XTL" "XTN" "XWEB"))
+
+(define delay-interval 10)
+
+(define delays (map (λ (x) (* delay-interval x)) (range 0 (length spdr-etfs))))
+
+(with-task-server (for-each (λ (l) (schedule-delayed-task (λ () (thread (λ () (download-etf-holdings (first l)))))
+                                                          (second l)))
+                            (map list spdr-etfs delays))
+  ; add a final task that will halt the task server
+  (schedule-delayed-task (λ () (schedule-stop-task)) (* delay-interval (length delays)))
+  (run-tasks))
